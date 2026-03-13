bf83984e8e1289c715a26cd8c7aea44c537649cb
SnifferGuardian <sbk322006@kis.ac.th>
2026-03-13 21:55:16 +0700

    added power and reaction time monitoring

--- file.py
+++
@@ -1,9 +0,0 @@
-from pathlib import Path
-file = "data.txt"
-def get_absolute_path(file_name: str) -> str:
-    path_object = Path(file_name)
-    return str(path_object.resolve())
-
-full_path = get_absolute_path("")
-abs_path=f"{full_path}\GeometryDash\{selected_files}"
-print(f"The absolute path is: {abs_path}")
